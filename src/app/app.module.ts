949714c0e38a0d6f2445b259a38a6e990aa6d75c
Sai Kumar Palreddy <76553984+palreddysai@users.noreply.github.com>
2023-02-03 14:01:30 -0600

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from 'src/app/shared/material/angular-material.module';
+import { LoginPageComponent } from './UI/login-page/login-page.component';
+import { HeaderComponent } from './UI/header/header.component';
+import { FooterComponent } from './UI/footer/footer.component';
+import { MainComponent } from './UI/main/main.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
